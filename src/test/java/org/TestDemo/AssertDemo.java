a9c893e687ee7ae2877abc4411240ebc7b057d25
Lsz19960724 <2049912588@qq.com>
2020-01-10 12:29:36 +0800

    2020/1/10

---
+++ src/test/java/org/TestDemo/AssertDemo.java
@@ -0,0 +1,30 @@
+package org.TestDemo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AssertDemo {
+	@Test
+	public void test1() {
+		Assert.assertEquals(1,2);
+	}
+	@Test
+	public void test2() {
+		Assert.assertEquals(1,1);
+	}
+	@Test
+	public void test3() {
+		Assert.assertEquals("aa","aa");
+	}
+//	@Test
+//	public void test4() {
+//		Assert.assertEquals(1,2);
+//	}
+	@Test
+	public void logDemo() {
+		Reporter.log("这是我自己写的日志");
+		throw new RuntimeException("这是我自己抛的异常");
+	}
+
+}
